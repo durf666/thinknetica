0f2571549bff0d7994d04ea09873e6011d83d495
smith <user@localhost.localdomain>
2016-04-26 08:27:38 -0400

    итерация 3

--- lesson2/shopping.rb
+++ lesson2/shopping.rb
@@ -1,6 +1,7 @@
 cart = {}
 total = 0
 loop do
+
   print "Введите имя товара: "
   name = gets.chomp
   break if name == 'стоп'
@@ -8,14 +9,11 @@ loop do
   cost = gets.chomp.to_i
   print "Введите количество товара: "
   number = gets.chomp.to_i
-  names << name
-  costs << cost
-  numbers << number
+
+  total += cost * number
+  cart [name]={cost: cost, number: number}
 end
 
-names.each_with index do |name, index|
-  summ = cost[index] * number[index]
-  total += summ
-  cart[name]= summ
 p cart
-p "Общая стоимость корзины покупок #{total}"
+cart.each {|name| p "Товар: #{name[0]} стоимость: #{name[1][:cost] * name[1][:number]}"}
+p "Итоговая сумма покупок: " + total.to_s
