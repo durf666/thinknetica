48de33d5887f0a7fbdaec2fb688aa8050114bbc0
durf666 <durf@inbox.ru>
2016-05-22 16:04:52 +0300

    усложннка 5

--- lesson5/instance_counter.rb
+++ lesson5/instance_counter.rb
@@ -8,15 +8,12 @@ module InstanceCounter
   end
 
   module ClassMethods
-
-    def instances
-      puts @instances
-    end
+    attr_accessor :instances
     
-    def register_instance
-      @instances||= 0
-      @instances+= 1
-    end  
+    # def register_instance
+    #   @instances||= 0
+    #   @instances+= 1
+    # end  
     
   
   end
@@ -24,9 +21,16 @@ module InstanceCounter
 
   module InstanceMethods
 
+    protected
+    def register_instance
+      self.class.instances||=0
+      self.class.instances+=1
+    end
     def initialize(name)
       super
-      self.class.register_instance
+      # self.class.register_instance
+      register_instance
+
     end
 
   end
